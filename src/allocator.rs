a796129348e9ce7022a7b7d059df0c4e1e72d030
Lucas Raoni <lucasraoniha@gmail.com>
2023-04-07 17:06:39 -0300

    Added diverse methods of heap management

--- src/allocator.rs
+++ src/allocator.rs
@@ -4,7 +4,14 @@ use x86_64::{
     },
     VirtAddr
 };
-use linked_list_allocator::LockedHeap;
+// use linked_list_allocator::LockedHeap;
+// use bump::BumpAllocator;
+// use linked_list::LinkedListAllocator;
+use fixed_size_block::FixedSizeBlockAllocator;
+
+pub mod bump;
+pub mod linked_list;
+pub mod fixed_size_block;
 
 // If we need more heap space in the future just change the HEAP_END
 // to a bigger number.
@@ -40,5 +47,56 @@ pub fn init_heap(
     Ok(())
 }
 
+// Imported crate heap allocation with linked_list_allocator
+// #[global_allocator]
+// static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+// Bump allocator implementation
+// Has a problem with long lived heap values. It can be fixed, but only for a problem and not 
+// the whole. So this is a use case specific.
+// #[global_allocator]
+// static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+
+// Linked List Allocator implementation 
+// problem with this allocation is without a agrouping of heap, so in a future, the heap will be 
+// so fragmented that it will not be able to allocate anything more. The imported crate has a 
+// implementation to reagroup and dont have this issue
+// #[global_allocator]
+// static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
+
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAllocator::new());
+
+// A wrapper around spin::Mutex to permit trait implementations
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
+
+// slower version of align_up
+// fn align_up(addr: usize, align: usize) -> usize {
+//     let remainder = addr % align;
+//     if remainder == 0 {
+//         addr
+//     } else {
+//         addr - remainder + align
+//     }
+// }
+
+// Align the given address 'addr' upwards to alignment 'align'
+//
+// Requires that 'align' is a power of two.
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
+}
